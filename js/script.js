36454d42554254ad21306d1075815b3aab500677
DebugHyena <maciej.staniec@yahoo.co.uk>
2023-11-08 07:40:28 +0000

    commit

--- js/script.js
+++ js/script.js
@@ -6,15 +6,17 @@ const closeButton = document.querySelector("ion-icon[name='close-outline']");
 
 btnMobileNav.addEventListener("click", function () {
   console.log("mobile nav clicked!");
-  if (
-    window.getComputedStyle(closeButton).getPropertyValue("display") === "none"
-  ) {
-    // Add the "nav-open" class to the header
-    header.classList.add("nav-open");
-  } else {
-    // Remove the "nav-open" class from the header
-    header.classList.remove("nav-open");
-  }
+
+  header.classList.toggle("nav-open");
+  // if (
+  //   window.getComputedStyle(closeButton).getPropertyValue("display") === "none"
+  // ) {
+  //   // Add the "nav-open" class to the header
+  //   header.classList.add("nav-open");
+  // } else {
+  //   // Remove the "nav-open" class from the header
+  //   header.classList.remove("nav-open");
+  // }
 });
 
 const footerYear = document.querySelector(".year");
